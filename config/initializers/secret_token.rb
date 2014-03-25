b5e97672f339a2fdf2da46ec98ddd87ec0cb3b88
GorawadeV <gorawadev@planit.net.au>
2014-03-24 20:02:13 -0700

    Initial application template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewRailsApp::Application.config.secret_key_base = '037f2a5cd02943da48b94b7b0e0561abfd90fd4e9e19ca1f7c2a76c4ae2cd9c04e90501d034ec7837124db2aacdfe2ea884db1f3a818ea367104f1675ca651c1'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+NewRailsApp::Application.routes.draw do
+  root to: 'home#index'
+
+  get "/" => 'home#index'
+  get "version" => 'version#index'
+  get "healthcheck" => 'healthcheck#index'
+
+end
